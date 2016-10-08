a15ba7e1afd8f7b08203a6bbb0c2fbaf73b331eb
Jordan Lowe <contact@jtlowe.com>
2016-10-07 22:16:08 -0500

    Config setup

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+/**
+ * @license
+ * Copyright Jordan Lowe. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+'use strict';
+
+var gulp = require("gulp");
+var ts = require("gulp-typescript");
+var tsProject = ts.createProject("tsconfig.json");
+
+gulp.task("default", function () {
+    return tsProject.src()
+        .pipe(tsProject())
+        .js.pipe(gulp.dest("dist"));
+});
