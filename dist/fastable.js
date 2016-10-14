ce394fbb31e53ef7a49fc654efef5cc4051ec3d4
Jordan Lowe <mail@jtlowe.com>
2016-10-14 06:34:53 -0500

    Adding code to create default thead from data

--- dist/fastable.js
+++ dist/fastable.js
@@ -3,9 +3,28 @@ var Fastable = (function () {
         if (options === void 0) {
             return;
         }
-        this.containerElement = options.containerElement;
+        this.container = options.container;
         this.data = options.data;
-        console.log(this.data);
+        this.create();
     }
+    Fastable.prototype.create = function () {
+        this.table = document.createElement('table');
+        this.table.setAttribute('class', 'fastable table table-bordered');
+        this.container.appendChild(this.table);
+        this.createHeader();
+    };
+    Fastable.prototype.createHeader = function () {
+        var thead = document.createElement('thead');
+        var tr = document.createElement('tr');
+        var keys = Object.keys(this.data[0]);
+        thead.appendChild(tr);
+        var length = keys.length;
+        for (var i = 0; i < length; i++) {
+            var td = document.createElement('td');
+            td.textContent = keys[i];
+            tr.appendChild(td);
+        }
+        this.table.appendChild(thead);
+    };
     return Fastable;
 }());
--- src/fastable.ts
+++ src/fastable.ts
@@ -1,18 +1,44 @@
 interface FastableOptions {
-    containerElement: HTMLElement;
+    container: HTMLElement;
     data: Object;
 }
 
 class Fastable {
-    containerElement: HTMLElement;
-    tableElement: HTMLElement;
+    container: HTMLElement;
+    table: HTMLElement;
     data: Object;
 
     constructor(options: FastableOptions) {
-        if (options === void 0) { return }
-        this.containerElement = options.containerElement;
+        if (options === void 0) { return; }
+        this.container = options.container;
         this.data = options.data;
 
-        console.log(this.data)
+        this.create();
+    }
+
+    create() {
+        this.table = document.createElement('table');
+        this.table.setAttribute('class', 'fastable table table-bordered');
+        this.container.appendChild(this.table);
+
+        this.createHeader();
+    }
+
+    createHeader() {
+        var thead: HTMLElement = document.createElement('thead');
+        var tr: HTMLTableRowElement = document.createElement('tr');
+        var keys: Array<string> = Object.keys(this.data[0]);
+
+        thead.appendChild(tr);
+
+        var length = keys.length;
+
+        for (var i = 0; i < length; i++) {
+            var td = document.createElement('td');
+            td.textContent = keys[i];
+            tr.appendChild(td);
+        }
+
+        this.table.appendChild(thead);
     }
 }
