7d3ba97e2a41aa5505344ce04a60dbecc028b113
Jordan Lowe <jordan@noreply.jtlowe.com>
2016-11-09 20:54:26 -0600

    Adding initial paging code

--- dist/fastable.js
+++ dist/fastable.js
@@ -1,17 +1,28 @@
 var Fastable = (function () {
     function Fastable(options) {
+        this.currentPage = 1;
         if (options === void 0) {
             return;
         }
         this.container = options.container;
         this.data = options.data;
+        this.limit = options.limit != undefined ? options.limit : 0;
         this.create();
     }
     Fastable.prototype.create = function () {
+        console.time();
         this.table = document.createElement('table');
         this.table.setAttribute('class', 'fastable table table-bordered');
         this.container.appendChild(this.table);
+        this.dom = {
+            type: 'table', props: { 'class': 'fastable table table-bordered' }, children: []
+        };
         this.createHeader();
+        this.createRows();
+        if (this.limit > 0) {
+            this.createPaging();
+        }
+        console.timeEnd();
     };
     Fastable.prototype.createHeader = function () {
         var thead = document.createElement('thead');
@@ -25,6 +36,57 @@ var Fastable = (function () {
             tr.appendChild(td);
         }
         this.table.appendChild(thead);
+    };
+    Fastable.prototype.shouldPage = function () {
+        var pageIt = false;
+        if (this.limit > 0 && this.data.length > this.limit) {
+            pageIt = true;
+        }
+        return pageIt;
+    };
+    Fastable.prototype.createPaging = function () {
+        var pagingList = document.createElement("ul");
+        var pageCount = Math.ceil(this.data.length / this.limit);
+        pagingList.className = "pagination";
+        for (var i = 1; i <= pageCount; i++) {
+            var listItem = document.createElement("li");
+            var link = document.createElement('a');
+            link.textContent = i.toString();
+            listItem.appendChild(link);
+            pagingList.appendChild(listItem);
+        }
+        this.container.appendChild(pagingList);
+        pagingList.addEventListener("click", function (e) {
+            if (e.srcElement && e.srcElement.nodeName == "A") {
+                // List item found!  Output the ID!
+                console.log("List item was clicked!");
+            }
+        });
+    };
+    Fastable.prototype.createRows = function () {
+        var tbody = document.createElement('tbody');
+        // var length: number = this.shouldPage() ? this.currentPage * this.data.length + this.limit : this.data.length;
+        var length = this.data.length;
+        var pageLength = this.shouldPage() ? this.limit : length;
+        var startIndex = this.currentPage * this.limit;
+        console.log(length, pageLength, startIndex);
+        for (var i = 0; i < pageLength; i++) {
+            var row = this.createRow(this.data[startIndex + i]);
+            tbody.appendChild(row);
+        }
+        this.table.appendChild(tbody);
+    };
+    Fastable.prototype.createRow = function (data) {
+        var row = document.createElement('tr');
+        var keys = Object.keys(data);
+        var i = keys.length;
+        while (i--) {
+            var position = 5 - i;
+            var cell = document.createElement('td');
+            cell.textContent = data[keys[position]];
+            row.appendChild(cell);
+        }
+        return row;
     };
     return Fastable;
 }());
--- src/fastable.ts
+++ src/fastable.ts
@@ -1,27 +1,44 @@
 interface FastableOptions {
     container: HTMLElement;
-    data: Object;
+    data: Array<Object>;
+    limit: number;
 }
 
 class Fastable {
     container: HTMLElement;
-    table: HTMLElement;
-    data: Object;
+    table: HTMLTableElement;
+    data: Array<Object>;
+    dom: Object;
+    limit: number;
+    currentPage: number = 1;
 
     constructor(options: FastableOptions) {
         if (options === void 0) { return; }
         this.container = options.container;
         this.data = options.data;
+        this.limit = options.limit != undefined ? options.limit : 0;
 
         this.create();
     }
 
     create() {
+        console.time();
         this.table = document.createElement('table');
         this.table.setAttribute('class', 'fastable table table-bordered');
         this.container.appendChild(this.table);
 
+        this.dom = {
+            type: 'table', props: { 'class': 'fastable table table-bordered' }, children: []
+        }
+
         this.createHeader();
+        this.createRows();
+
+        if (this.limit > 0) {
+            this.createPaging();
+        }
+
+        console.timeEnd();
     }
 
     createHeader() {
@@ -40,5 +57,74 @@ class Fastable {
         }
 
         this.table.appendChild(thead);
+    }
+
+    shouldPage(): boolean {
+        var pageIt: boolean = false;
+
+        if (this.limit > 0 && this.data.length > this.limit) {
+            pageIt = true
+        }
+
+        return pageIt;
+    }
+
+    createPaging() {
+        var pagingList = document.createElement("ul");
+        var pageCount: number = Math.ceil(this.data.length / this.limit);
+
+        pagingList.className = "pagination";
+
+        for (var i = 1; i <= pageCount; i++) {
+            var listItem = document.createElement("li");
+            var link = document.createElement('a');
+            link.textContent = i.toString();
+
+            listItem.appendChild(link);
+            pagingList.appendChild(listItem);
+        }
+
+        this.container.appendChild(pagingList);
+
+        pagingList.addEventListener("click", function (e: Event): void {
+            if (e.srcElement && e.srcElement.nodeName == "A") {
+                // List item found!  Output the ID!
+                console.log("List item was clicked!");
+            }
+        });
+    }
+
+    createRows() {
+        var tbody = document.createElement('tbody');
+        // var length: number = this.shouldPage() ? this.currentPage * this.data.length + this.limit : this.data.length;
+
+        var length = this.data.length;
+        var pageLength = this.shouldPage() ? this.limit : length;
+        var startIndex = this.currentPage * this.limit;
+
+        console.log(length, pageLength, startIndex)
+
+        for (var i = 0; i < pageLength; i++) {
+            var row: HTMLTableRowElement = this.createRow(this.data[startIndex + i]);
+            tbody.appendChild(row);
+        }
+
+        this.table.appendChild(tbody);
+    }
+
+    createRow(data: Object): HTMLTableRowElement {
+        var row: HTMLTableRowElement = document.createElement('tr');
+
+        var keys = Object.keys(data)
+        var i = keys.length;
+
+        while (i--) {
+            var position = 5 - i;
+            var cell = document.createElement('td');
+            cell.textContent = data[keys[position]];
+            row.appendChild(cell);
+        }
+
+        return row;
     }
 }
